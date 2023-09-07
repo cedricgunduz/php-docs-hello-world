b30d9abc1c257d40daa9b161d1b4ba394ea59951
cedricgunduz <144235523+cedricgunduz@users.noreply.github.com>
2023-09-07 08:11:41 +0200

    Update Test_Php2.php

--- Test_Php2.php
+++ Test_Php2.php
@@ -7,6 +7,7 @@ $dbpass = "5JZ`x8\D&^0pbADKR`+n";
 $db = "db-mariadb-7930-uat-db01test";
 $conn = new mysqli($dbhost, $dbuser, $dbpass,$dbname) or die("Connect failed: %s\n". $conn -> error);
 return $conn;
+echo "Hello World!";
 }
 function CloseCon($conn)
 {
