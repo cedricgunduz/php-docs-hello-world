824cce53b88ab458fb8aff627301eea2e03a1da8
cedricgunduz <144235523+cedricgunduz@users.noreply.github.com>
2023-09-07 08:58:03 +0200

    Update Test_Php2.php

--- Test_Php2.php
+++ Test_Php2.php
@@ -5,14 +5,13 @@ function OpenCon()
 $dbhost = "db-mariadb-7930-uat-db01test.mariadb.database.azure.com";
 $dbuser = "adminebrc@db-mariadb-7930-uat-db01test";
 $dbpass = "5JZ`x8\D&^0pbADKR`+n";
-$dbname = "db-mariadb-7930-uat-db01test";
+$dbname = "mysql";
 $conn = new mysqli($dbhost, $dbuser, $dbpass,$dbname) or die("Connect failed: %s\n". $conn -> error);
-return $conn;
-echo "Hello World2";
+echo "Accès OK à la base de données";
 }
 function CloseCon($conn)
 {
 $conn -> close();
 }
-OpenCon();
+opencon()
 ?>
