eacbe63c1c856af01fa024e8ee06803de5163886
cedricgunduz <144235523+cedricgunduz@users.noreply.github.com>
2023-09-08 07:41:06 +0200

    Add files via upload

---
+++ Test_Php3.php
@@ -0,0 +1,17 @@
+<?php
+echo "Hello World1";
+function OpenCon()
+{
+$dbhost = "mdbsrv-002.mariadb.database.azure.com";
+$dbuser = "admineos@mdbsrv-002";
+$dbpass = "5JZ`x8\D&^0pbADKR`+n";
+$dbname = "mysql";
+$conn = new mysqli($dbhost, $dbuser, $dbpass,$dbname) or die("Connect failed: %s\n". $conn -> error);
+echo "Accès OK à la base de données";
+}
+function CloseCon($conn)
+{
+$conn -> close();
+}
+opencon()
+?>
