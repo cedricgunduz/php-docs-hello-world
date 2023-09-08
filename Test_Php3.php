6f4f6bb9271f7300ffa4e271dc11ee6239b9d1ec
cedricgunduz <144235523+cedricgunduz@users.noreply.github.com>
2023-09-08 07:49:47 +0200

    Update Test_Php3.php

--- Test_Php3.php
+++ Test_Php3.php
@@ -3,7 +3,7 @@ echo "Hello World1";
 function OpenCon()
 {
 $dbhost = "mdbsrv-002.mariadb.database.azure.com";
-$dbuser = "admineos@mdbsrv-002";
+$dbuser = "admineos+@mdbsrv-002";
 $dbpass = "5JZ`x8\D&^0pbADKR`+n";
 $dbname = "mysql";
 $conn = new mysqli($dbhost, $dbuser, $dbpass,$dbname) or die("Connect failed: %s\n". $conn -> error);
